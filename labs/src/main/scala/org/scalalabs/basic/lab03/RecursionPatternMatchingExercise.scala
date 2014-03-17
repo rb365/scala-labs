ba5ce7292d4d9a926ab4b9aace648b6d1bdd7cbc
vskarine <skarine@gmail.com>
2014-03-16 19:44:05 -0700

    basic lab03: recursion pattern matching exercise

--- labs/src/main/scala/org/scalalabs/basic/lab03/RecursionPatternMatchingExercise.scala
+++ labs/src/main/scala/org/scalalabs/basic/lab03/RecursionPatternMatchingExercise.scala
@@ -28,56 +28,64 @@ object RecursionPatternMatchingExercise {
    * checkValuesIncrease(Seq(1,2,3)) == true
    * checkValuesIncrease(Seq(1,2,2)) == false
    */
-  def checkValuesIncrease(seq: Seq[Int]): Boolean = {
-    error("fix me")
+  def checkValuesIncrease(seq: Seq[Int]): Boolean = seq match {
+    case seq: Seq[Int] if seq.length <= 1 => true
+    case seq: Seq[Int] if seq(0) < seq(1) => checkValuesIncrease(seq.drop(1))
+    case _ => false
   }
   
   /**
    * Group Consecutive values
    * List(1,1,2,3,1,1) -> List(1,1), List(2), List(3), List(1,1)
    */
-  def groupConsecutive[T](in: List[T]): List[List[T]] = {
-    error("fix me")
+  def groupConsecutive[T](in: List[T]): List[List[T]] = in match {
+    case in:List[T] if in.isEmpty => List[List[T]]()
+    case _ => in.takeWhile(_ == in(0)) :: groupConsecutive(in.dropWhile(_ == in(0)))
   }
 
   /**
    * Group Equal values
    * List(1,1,2,3,1,1) -> List(1,1,1,1), List(2), List(3)
    */
-  def groupEquals[T](in: List[T]): List[List[T]] = {
-    error("fix me")
+  def groupEquals[T](in: List[T]): List[List[T]] = in match {
+    case in:List[T] if in.isEmpty => List[List[T]]()
+    case _ => in.filter(_ == in(0)) :: groupEquals(in.filter(_ != in(0)))
   }
 
   /**
    * Compress values
    * List(1,1,2,3,1,1) -> List(1,2,3)
    */
-  def compress[T](in: List[T]): List[T] = {
-    error("fix me")
+  def compress[T](in: List[T]): List[T] = in match {
+    case in:List[T] if in.isEmpty => List[T]()
+    case _ => in(0) :: compress(in.dropWhile(_ == in(0)))
   }
   
   /**
    * Define the amount of all equal members
    * List(1,1,2,3,1,1) -> List((4,1),(1,2),(1,3))
    */
-  def amountEqualMembers[T](in: List[T]): List[(Int, T)] = {
-    error("fix me")
+  def amountEqualMembers[T](in: List[T]): List[(Int, T)] = in match {
+    case in:List[T] if in.isEmpty => List[(Int, T)]()
+    case _ => (in.filter(_ == in(0)).size, in(0)) :: amountEqualMembers(in.filter(_ != in(0)))
   }
   
   /**
    * Zip multiple lists
    * List(List(1,2,3), List('A, 'B, 'C), List('a, 'b, 'c)) -> List(List(1, 'A, 'a), List(2, 'B, 'b), List(3, 'C, 'c))
    */
-  def zipMultiple(in: List[List[_]]): List[List[_]] = {
-    error("fix me")
+  def zipMultiple(in: List[List[_]]): List[List[_]] = in match {
+    case in: List[List[_]] if in.isEmpty => List[List[_]]()
+    case in: List[List[_]] if in.length == 1 => in(0).map(List(_))
+    case _ => in(0).zip(zipMultiple(in.drop(1))).map(t => t._1 :: t._2)
   }
 
-  /**
+    /**
    * Zip multiple lists with different sizes
    * List(List(1), List('A, 'B, 'C), List('a, 'b)) -> List(List(1, 'A, 'a))
    */
-  def zipMultipleWithDifferentSize(in: List[List[_]]): List[List[_]] = {
-    error("fix me")
+  def zipMultipleWithDifferentSize(in: List[List[_]]): List[List[_]] =  {
+      zipMultiple(in)
   }
 
 }
