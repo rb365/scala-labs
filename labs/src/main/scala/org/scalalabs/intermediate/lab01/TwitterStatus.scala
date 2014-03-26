1fd18a2063de160056193ab0a9e00310e587d2c8
vskarine <skarine@gmail.com>
2014-03-25 23:40:44 -0700

    intermediate lab01: not robust but works!

--- labs/src/main/scala/org/scalalabs/intermediate/lab01/TwitterStatus.scala
+++ labs/src/main/scala/org/scalalabs/intermediate/lab01/TwitterStatus.scala
@@ -1,2 +1,36 @@
 package org.scalalabs.intermediate.lab01
 
+import org.joda.time.DateTime
+import org.joda.time.format.DateTimeFormat
+import java.util.Locale
+
+class TwitterStatus {
+  var id: Long = -1
+  var inReplyToStatusId: Any = None
+  var inReplyToUserId: Any = None
+  var truncated: Boolean = false
+  var favorited: Boolean = false
+  var text: String = ""
+  var createdAt: DateTime = null
+  var user: TwitterUser = null
+
+  def this(n : scala.xml.Node) = {
+    this
+    id = (n\"id")(0).text.toLong
+    var tmp = (n\"in_reply_to_status_id")(0).text
+    inReplyToStatusId = if (tmp.isEmpty) None else tmp.toLong
+    tmp = (n\"in_reply_to_user_id")(0).text
+    inReplyToUserId = if (tmp.isEmpty) None else tmp.toLong
+    truncated = (n\"truncated")(0).text.toBoolean
+    favorited = (n\"favorited")(0).text.toBoolean
+    text = (n\"text")(0).text
+    createdAt = TwitterStatus.twitterDateTimeFormat.parseDateTime((n\"created_at")(0).text)
+    user = new TwitterUser((n\\"user")(0))
+  }
+
+}
+
+object TwitterStatus {
+  val twitterDateTimeFormat = DateTimeFormat.forPattern("EE MMM dd HH:mm:ss Z yyyy").withLocale(Locale.US)
+
+}
--- labs/src/main/scala/org/scalalabs/intermediate/lab01/TwitterUser.scala
+++ labs/src/main/scala/org/scalalabs/intermediate/lab01/TwitterUser.scala
@@ -1,3 +1,29 @@
 package org.scalalabs.intermediate.lab01
 
 
+class TwitterUser {
+  var id: Long = -1
+  var name: String = ""
+  var screen_name: String = ""
+  var description: String = ""
+  var location: String = ""
+  var url: String = ""
+  var profileImageUrl: String = ""
+  var statusesCount: Long = -1
+  var friendsCount: Long = -1
+  var followersCount: Long = -1
+
+  def this(n : scala.xml.Node) = {
+    this
+    id = (n\"id")(0).text.toLong
+    name = (n\"name")(0).text
+    screen_name = (n\"screen_name")(0).text
+    description = (n\"description")(0).text
+    location = (n\"location")(0).text
+    url = (n\"url")(0).text
+    profileImageUrl = (n\"profile_image_url")(0).text
+    statusesCount = (n\"statuses_count")(0).text.toLong
+    friendsCount = (n\"friends_count")(0).text.toLong
+    followersCount = (n\"followers_count")(0).text.toLong
+  }
+}
